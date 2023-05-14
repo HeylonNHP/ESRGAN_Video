c5dc23e1afa4b44247f084d179f0342eec71a7e3
Heylon White <heylon96@hotmail.com>
2023-05-14 19:23:53 +1000

    Initial setup

--- main.py
+++ main.py
@@ -1,34 +1,52 @@
 import cv2
+import os
+import sys
 
-# Open the video file
-cap = cv2.VideoCapture(r'N:\video\cat-cats.gif')
+sys.path.insert(0, os.getcwd() + os.path.sep + 'esrgan')
+import esrgan.upscale
 
-# Set the frame counter to 0
-frame_count = 0
 
-# Loop through the video frames
-while cap.isOpened():
+class esrgan_video_upscaler():
 
-    # Read a frame
-    ret, frame = cap.read()
+    def upscale_video(self):
+        upscaler = esrgan.upscale.Upscale()
+        # Open the video file
+        cap = cv2.VideoCapture(r'N:\video\cat-cats.gif')
 
-    # If the frame was read successfully
-    if ret:
+        # Set the frame counter to 0
+        frame_count = 0
 
-        # Show the frame
-        cv2.imshow('Frame', frame)
+        # Loop through the video frames
+        while cap.isOpened():
 
-        # Increment the frame counter
-        frame_count += 1
+            # Read a frame
+            ret, frame = cap.read()
 
-        # If we have shown 5 frames, exit the loop
-        if frame_count == 5:
-            break
+            # If the frame was read successfully
+            if ret:
 
-    # If the frame was not read successfully, exit the loop
-    else:
-        break
+                # Show the frame
+                cv2.imshow('Frame', frame)
 
-# Release the video file and destroy the window
-cap.release()
-cv2.destroyAllWindows()
+                # Increment the frame counter
+                frame_count += 1
+
+                # If we have shown 5 frames, exit the loop
+                if frame_count == 5:
+                    break
+
+            # If the frame was not read successfully, exit the loop
+            else:
+                break
+
+        # Release the video file and destroy the window
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+def main():
+    upscaler = esrgan_video_upscaler()
+    upscaler.upscale_video()
+
+
+main()
---
+++ test/testmod.py
@@ -0,0 +1,4 @@
+class testmod:
+
+    def __init__(self):
+        print("Deez nuts")
