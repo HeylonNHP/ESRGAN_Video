7e9c8129ffea1c6062445a9c23ed133372b94b45
Heylon White <heylon96@hotmail.com>
2023-05-15 20:15:31 +1000

    Take video path as argument

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ class esrgan_video_upscaler():
     model_scale_factor = 4
     model_name = r"esrgan\models\4x-UltraSharp-upscale-jpeg-images.pth"
 
-    def upscale_video(self):
+    def upscale_video(self, video_path):
         upscaler = esrgan.upscale.Upscale(input=Path("input"), output=Path("output"), model=self.model_name,
                                           alpha_mode=esrgan.upscale.AlphaOptions.SWAPPING)
         upscaler.load_model(self.model_name)
         # Open the video file
-        cap = cv2.VideoCapture(r'N:\video\cat-cats.gif')
+        cap = cv2.VideoCapture(video_path)
         fps = cap.get(cv2.CAP_PROP_FPS)
         width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -55,8 +55,9 @@ class esrgan_video_upscaler():
 
 
 def main():
+    video_input_path = sys.argv[1]
     upscaler = esrgan_video_upscaler()
-    upscaler.upscale_video()
+    upscaler.upscale_video(video_input_path)
 
 
 main()
