55f6db41b6663de3ea629af6e96925365de5a5e5
Heylon White <heylon96@hotmail.com>
2023-05-15 22:42:48 +1000

    Add ability to set downscaled output scale

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import cv2
 import os
 import sys
 
+from cv2 import VideoWriter
+
 sys.path.insert(0, os.getcwd() + os.path.sep + 'esrgan')
 import esrgan.upscale
 
@@ -28,19 +30,31 @@ class esrgan_video_upscaler():
 
         # Open the output video file
         fourcc = cv2.VideoWriter_fourcc(*'FFV1')
-        out = cv2.VideoWriter('output.mkv', fourcc, fps,
-                              (width * self.model_scale_factor, height * self.model_scale_factor))
+        out: VideoWriter = None
+        calculated_scaling_factor: int
 
         # Loop through the video frames
         while cap.isOpened():
-
             # Read a frame
             ret, frame = cap.read()
 
             # If the frame was read successfully
             if ret:
-
                 upscaled_frame = upscaler.upscale(frame)
+
+                if out is None:
+                    # Initialise output once the output resolution is known
+                    upscaled_frame_height, upscaled_frame_width = upscaled_frame.shape[:2]
+                    calculated_scaling_factor = ((upscaled_frame_height / height) + (
+                            upscaled_frame_width / width)) / 2.0
+                    out = cv2.VideoWriter('output.mkv', fourcc, fps,
+                                          (int(width * self.model_scale_factor),
+                                           int(height * self.model_scale_factor)))
+
+                if self.model_scale_factor < calculated_scaling_factor:
+                    scale = float(self.model_scale_factor) / float(calculated_scaling_factor)
+                    upscaled_frame = cv2.resize(upscaled_frame, (0, 0), fx=scale, fy=scale,
+                                                interpolation=cv2.INTER_AREA)
                 out.write(cv2.convertScaleAbs(upscaled_frame, alpha=(1)))
 
             # If the frame was not read successfully, exit the loop
